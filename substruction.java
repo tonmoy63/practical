20ba3ce304e632397b5528bda51f43d2292031f9
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-22 13:47:30 +0530

    Add files via upload

---
+++ substruction.java
@@ -0,0 +1,10 @@
+public class Djava {
+
+   public static void main(String[] args) {
+        
+      int a = 15, b = 5, sub;
+      sub = a - b;
+
+      System.out.println("Sub of these numbers: "+sub);
+   }
+}
