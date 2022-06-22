4a85121c4fb6ebe1bda57c996a40eb9f27cb2541
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-22 20:21:27 +0530

    Add files via upload

---
+++ asmd.java
@@ -0,0 +1,17 @@
+
+public class asmd
+{
+   public static void main(String[] args)
+   {
+      int num1 = 20, num2 = 15, add, sub, multiply, devide;
+      
+      add = num1 + num2;
+      System.out.println("Addition  = " + add);
+     sub = num1 - num2;
+      System.out.println("Subtraction  = " + sub);
+    multiply = num1 * num2;
+      System.out.println("Multiplication  = " + multiply);
+    devide = num1 / num2;
+      System.out.println("Division  = " + devide);
+   }
+}
