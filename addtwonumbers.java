172dad6d2d7ffd7d74b464ce750ef2ecf00572fd
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 11:30:51 +0530

    Update and rename Djava.java to addtwonumbers.java

---
+++ addtwonumbers.java
@@ -0,0 +1,12 @@
+/* Author: Tonmoy Halder
+public class addtwonumbrers {
+
+   public static void main(String[] args) {
+        
+      int a = 5,    //using int data type and variable name is a
+      int b = 15, sum;  //using int data type and variable name is b
+      sum = a + b; //adding two numbers
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
