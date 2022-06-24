1806b5aac3014a7410b626f1802fbdc5dc79a647
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 11:33:00 +0530

    Update multiplication.java

--- multiplication.java
+++ multiplication.java
@@ -2,10 +2,10 @@ public class multiplication  {
 
    public static void main(String[] args) {
         
-      int a = 5; int b = 15;
+      int a = 5; int b = 15;// using data type int and variable a ana b
      
 
-      System.out.println(a*b);
+      System.out.println(a*b);//multiply two numbers
 
    }
 }
