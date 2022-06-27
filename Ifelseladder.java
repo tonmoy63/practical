01719143936c88a96ab076a6d0e8805e24d2d549
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-27 16:49:43 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,16 @@
+public class Ifelseladder {
+	public static void main(String[]args) {
+		String name = "Tonmoy";
+		if (name == "neha") {
+			System.out.println("name is neha");
+		}else if (name == "puja") {
+			System.out.println("name is puja");
+			
+		}else if (name == "amit") {
+			System.out.println("name is amit");
+		}else {
+			System.out.println("name");
+		}
+	}
+
+}
