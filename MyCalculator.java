c459cfcdfa87c5060a64c5f366dcae02688c429b
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-08-02 12:39:15 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,21 @@
+package Wrapper;
+
+public class MyCalculator {
+	public static long power(int n, int p) throws Exception{
+		if (n<0 || p<0) {
+			throw new Exception("n and p should not be negative");
+			
+		}
+		else if(n==0 && p==0) {
+			throw new Exception ("n and p should not be zero");
+			
+		}else {
+			return(long)Math.pow(n,p);
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		System.out.println(power(3,5));
+		
+	}
+
+}
